9c55dab5fdb5bd984ced2cd633bc3c19203b7b59
prashant00793 <prashantqa007@gmail.com>
2024-01-21 23:32:14 +0530

    apiautomation

--- src/main/java/com/apiautomationpk/utils/UtilExcel.java
+++ src/main/java/com/apiautomationpk/utils/UtilExcel.java
@@ -1,4 +1,33 @@
-package com.apiautomationpk.utils;
-
-public class UtilExcel {
-}
+//package com.apiautomationpk.utils;
+//
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+//import org.testng.annotations.DataProvider;
+//
+//import java.io.FileInputStream;
+//
+//public class UtilExcel {
+//    //FileInputStream
+//    //get the data into an Object [][]
+//
+//    static Workbook book;
+//    static Sheet sheet;
+//
+//    public static String Sheet_Name = System.getProperty("user.dir") + "/src/main/java/com/apiautomationpk/resource/TestData.xlsx";
+//    FileInputStream file = null;
+//    file  = new  FileInputStream(Sheet_Name);
+//}
+//
+//public static Object[][] getTestDataFromExcel(String SheetName) {
+//    return null;
+//
+//}
+//
+//@DataProvider
+//public Object[][] getData() {
+//    return getTestDataFromExcel();
+//}
+//}
+//
+//public void main() {
+//}
