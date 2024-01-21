314dce552963a6e43fe5cb41bb57134fd4cf59ad
prashant00793 <prashantqa007@gmail.com>
2024-01-21 22:52:24 +0530

    apiautomation

--- src/main/java/com/apiautomationpk/endpoints/APIConstants.java
+++ src/main/java/com/apiautomationpk/endpoints/APIConstants.java
@@ -1,10 +1,22 @@
 package com.apiautomationpk.endpoints;
 
+import com.apiautomationpk.utils.PropertyReaderUtil;
+
 public class APIConstants {
 
     //public static String BASE_URL= FillowUtils.fetchDataFromXLSX("Sheet1","BaseUrl","Value" );
 
-    public static String BASE_URL = "https://restful-booker.herokuapp.com/";
+    public static String BASE_URL;
+
+    static {
+        try {
+            BASE_URL = PropertyReaderUtil.readkey("url");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     public static String CREATE_BOOKING = "/booking";
     public static String UPDATE_BOOKING = "/booking";
 
--- src/main/java/com/apiautomationpk/utils/PropertyReaderUtil.java
+++ src/main/java/com/apiautomationpk/utils/PropertyReaderUtil.java
@@ -1,4 +1,31 @@
 package com.apiautomationpk.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyReaderUtil {
+
+    public PropertyReaderUtil() {
+
+    }
+
+    public static String readkey(String key) throws Exception {
+
+        FileInputStream fileInputStream = new FileInputStream(new File(System.getProperty("user.dir") + "/src/main/java/com/apiautomationpk/resource/TDP.properties"));
+        Properties p = new Properties();
+        p.load(fileInputStream);
+
+        if (p.getProperty(key) == null) {
+            throw new Exception("Not able to find the key");
+
+        } else {
+            return p.getProperty(key);
+        }
+    }
+
+
 }
+
