da2006b0be841294efc192b2410561f05a9e6a66
prashant00793 <prashantqa007@gmail.com>
2024-01-21 23:32:45 +0530

    apiautomation

--- src/test/java/com/apiautomationpk/DDT/LoginWithDDT.java
+++ src/test/java/com/apiautomationpk/DDT/LoginWithDDT.java
@@ -1,15 +1,15 @@
-package com.apiautomationpk.DDT;
-
-import com.apiautomationpk.utils.UtilExcel;
-import org.testng.annotations.Test;
-
-public class LoginWithDDT {
-
-    @Test(dataProvider = "getData", dataProviderClass = UtilExcel.class)
-    public void testLoginData(String username, String password){
-        System.out.println("Username__" + username);
-        System.out.println("Password__" + password);
-
-        //Rest Assured Code
-    }
-}
+//package com.apiautomationpk.DDT;
+//
+//import com.apiautomationpk.utils.UtilExcel;
+//import org.testng.annotations.Test;
+//
+//public class LoginWithDDT {
+//
+//    @Test(dataProvider = "getData", dataProviderClass = UtilExcel.class)
+//    public void testLoginData(String username, String password){
+//        System.out.println("Username__" + username);
+//        System.out.println("Password__" + password);
+//
+//        //Rest Assured Code
+//    }
+//}
